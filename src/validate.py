08e5550e09b6221f11a3ec8abbee6d9f973a2b7f
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-09-02 16:58:37 +0200

    feat: Added the starter for validator, added the validation for report id

---
+++ src/validate.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from src.extract import read_report
+
+
+
+# VALID — safe to use.
+# FLAGGED — data exists, but needs investigation.
+# INVALID — essential data is missing or unusable.
+
+# rules 
+
+    # Report_id - Required, Positive, unique
+    # area - Required , not null, match known Areas
+    # utilitty_type, Required, Electricity and water only
+    # status - Required, outage or restored
+    # reported at - valid_date and time , not be in future
+    # source, Not required
+
+def report_id_validator(row: pd.Series ) -> bool:
+
+    '''
+        Validate the report id only
+    '''
+
+    report_id = row.report_id
+    if pd.isna(report_id):
+        return False
+
+    if isinstance(report_id, float):
+        return False
+    try:
+        report_id = int(report_id)
+        if report_id < 0:
+            raise ValueError
+    except ValueError:
+        return False
+    return True
+
+def area_validator_check1(row: pd.Series) -> bool:
+    '''
+        Validate Area to check Null, Empty and return False
+    '''
+
+
+
+def area_validator_check2(row: pd.Series) -> str:
+    '''
+        Validate Area to check if the City Exists
+    '''
+
+    
+
+
+
+
+def validate(df : pd.DataFrame) -> None:
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    # file_name = "data/raw/outage_reports.csv"
+    # df = read_report(file_name)
+    # validate(df)
+    df = pd.DataFrame({
+    'report_id': [None, 'Bob'],
+    'Age': [25, 30],
+    'City': ['New York', 'Paris']
+    })
+    print(report_id_validator(df.iloc[0]))
